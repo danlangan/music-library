c03fb7bc90f603b647b497e9ded87816634dd399
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-08-19 15:08:50 -0500

    coded the URL pattern and edited the settings file, turned USE_TZ to false since it was giving my terminal a hard time. Also added a urls fule to the music library application folder, should have named that the application since the project is named pretty similar. Should be okay though

--- settings.py
+++ settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'music_library',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +102,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
